11a815648b15659b8ba5e18d30be99b96da9648b
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-11-02 08:47:24 +0000

    Add randomness

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
+import random
+
 def m():
-  print("Keep it logically awesome.")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  i = random.randint(0, len(quotes) - 1)
+  print(quotes[i])
 
 if __name__== "__main__":
   m()
